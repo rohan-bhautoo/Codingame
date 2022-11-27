280bbf0cbf95a21bd4208647c68bd93041e98eb2
Rohan Bhautoo <47154593+rohan-bhautoo@users.noreply.github.com>
2022-11-27 23:03:43 +0400

    Create Temperatures.java

---
+++ Easy/Temperatures/Temperatures.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // the number of temperatures to analyse
+        String TEMPS = in.nextLine(); // the N temperatures expressed as integers ranging from -273 to 5526
+        
+        if(n > 0 ) {
+            String list[] = in.nextLine().split(" ");
+       
+            int res = 10000;
+            int temp = 0;
+            for ( int i=0; i<list.length; i++ ) {
+                temp = Integer.parseInt(list[i]);
+                if ( Math.abs(temp) < Math.abs(res)) {
+                    res = temp;
+                } else if ( Math.abs(temp) == Math.abs(res) && temp != res){
+                    res = Math.abs(temp);
+                }
+            }
+            System.out.println(res);
+        } else {
+            System.out.println(0);
+        }  
+        in.close();
+    }
+    
+}
